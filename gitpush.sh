a046cf5a3cfa375b55035d910ab3287c09953f0a
Ian Bartholomew <ibartholomew@hugeinc.com>
2012-02-17 11:40:26 -0800

    adding script

---
+++ gitpush.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+RESET="\033[0m"
+REDCOLOR="\033[31m"
+GREENCOLOR="\033[32m"
+
+current_branch=`git branch | awk '/\*/ {print $2}'`
+
+clear
+echo -e $GREENCOLOR"--------------------------------------- STARTING PUSH -------------------------------------------"$RESET
+
+git checkout master && git pull origin master && git checkout $current_branch && git rb master && git checkout master && git pull origin master && git merge $current_branch && git push origin master && git checkout $current_branch || {
+	echo -e $REDCOLOR"----------------------------------- PUSH FAILED (SEE ERRORS)-------------------------------------"$RESET;
+	echo -e $REDCOLOR"---- Most likely, you will just need to run 'git mergetool' and then 'git rebase --continue' ----"$RESET;
+	exit 1;
+}
+
+echo -e $GREENCOLOR"--------------------------------- PUSH AND REBASE SUCCESSFUL ------------------------------------"$RESET
